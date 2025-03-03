7c2b365dbe641bef6992d9332f3ca5cdcb73cbea
Mathi031 <mathi031@hotmail.com>
2025-03-03 11:17:07 -0500

    feat: remove cors validation

--- src/server.ts
+++ src/server.ts
@@ -15,12 +15,7 @@ dotenv.config();
 
 const app = express();
 app.set("trust proxy", 1);
-const corsOptions = {
-  // origin: process.env.CORS_ORIGIN,
-  methods: "GET,POST,PUT,DELETE,OPTIONS",
-  allowedHeaders: "Content-Type,Authorization,Cache-Control,Timestamp",
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", authRoutes);
